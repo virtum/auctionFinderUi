dc417283d0eec949845a96d3788aefdf4b654607
Virtum <pawel.filocha@gmail.com>
2017-04-22 22:21:54 +0200

    Add basic functionality to send request to backend with given keyword

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Injectable()
 export class AppComponent {
-  title = 'app works!';
+  private response = '';
+  private values = '';
+  private url = 'http://localhost:8080/find';
+
+  constructor(private http: Http) { }
+
+  onKey(event: any) {
+    this.values = event.target.value;
+  }
+
+  onButtonClicked(event) {
+    this.sendItemName(this.values).subscribe(res => {
+      this.response = 'Response: ' + res;
+      console.log(res);
+      this.values = '';
+    }
+    );
+  }
+
+  sendItemName(name): Observable<String> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.url, { name }, options)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+
+  private extractData(res: Response) {
+    let body = res.json();
+    console.log('Response: ', body.response);
+
+    return body.response || {};
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
 }
