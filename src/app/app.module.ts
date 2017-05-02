24020bc9f3052bc7b408ed5d9fb4324664e9299d
Virtum <pawel.filocha@gmail.com>
2017-05-02 09:09:40 +0200

    Add two new components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FinderComponent } from './auctionfinder/finder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FinderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'find', component: FinderComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '**', redirectTo: 'login', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+
+@Component({
+    templateUrl: './login.component.html'
+})
+
+@Injectable()
+export class LoginComponent {
+
+}
