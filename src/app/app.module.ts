dc7d39e2c6f0691adff6235f95288a42263034fa
Virtum <pawel.filocha@gmail.com>
2017-05-11 22:25:52 +0200

    Add guard for routes and simple login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
+import { FacebookModule } from 'ngx-facebook';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FinderComponent } from './auctionFinder/finder.component';
+import { AuthGuard } from './guard/authGuard.service';
 
 @NgModule({
   declarations: [
@@ -18,14 +19,15 @@ import { FinderComponent } from './auctionFinder/finder.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    FacebookModule.forRoot(),
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
-      { path: 'find', component: FinderComponent },
+      { path: 'find', component: FinderComponent, canActivate: [AuthGuard] },
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: '**', redirectTo: 'login', pathMatch: 'full' }
     ])
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,69 @@
 import { Component } from '@angular/core';
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import { FacebookService, InitParams, LoginResponse } from 'ngx-facebook';
+import { AuthGuard } from '../guard/authGuard.service';
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
 })
 
 @Injectable()
 export class LoginComponent {
 
+    constructor(private fb: FacebookService, private http: Http, private guard : AuthGuard) {
+        console.log('login');
+        fb.init({
+            appId: '1722165054742491',
+            xfbml: true,
+            version: 'v2.8'
+        });
+    }
+
+    login() {
+        this.fb.login()
+            .then((res: LoginResponse) => {
+                console.log('Logged in', res);
+                console.log('Access token: ', res.authResponse.accessToken);
+                this.sendAccessToken(res.authResponse.accessToken).subscribe(res => {
+                    this.guard.isLogged = <any>res;
+                }
+                );
+            })
+            .catch(this.handleError);
+    }
+
+    sendAccessToken(accessToken): Observable<String> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.post('http://localhost:8080/login', { accessToken }, options)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    private extractData(res: Response) {
+        let body = res.json();
+        console.log('Response: ', body.response);
+
+        return body.response || {};
+    }
+
+    private handleError(error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
 }
