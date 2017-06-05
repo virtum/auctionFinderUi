1238388eb8839bfb33e13d254c440c37c5ae8f2b
Filocha <pafo@gft.com>
2017-06-05 12:17:55 +0200

    Add http get request for account data for further implementation

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
@@ -11,21 +11,28 @@ import { AuthGuard } from '../guard/authGuard.service';
 })
 
 @Injectable()
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+    private accountData: String;
 
     constructor(private http: Http, private guard: AuthGuard, private router: Router) { }
 
-    private handleError(error: Response | any) {
-        let errMsg: string;
-        if (error instanceof Response) {
-            const body = error.json() || '';
-            const err = body.error || JSON.stringify(body);
-            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-        } else {
-            errMsg = error.message ? error.message : error.toString();
-        }
-        console.error(errMsg);
-        return Observable.throw(errMsg);
+    ngOnInit() {
+        this.getUserSubscriptions().subscribe(res => {
+            console.log('account data', res.accountData);
+            this.accountData = res.accountData;
+        });
+    }
+
+    getUserSubscriptions() {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers, withCredentials: true });
+
+        return this.http.get('http://localhost:8080/rest/subscriptions', options)
+            .map(res => {
+                let body = res.json();
+                return body || {};
+            })
+            .catch(this.handleError);
     }
 
     logout() {
@@ -46,6 +53,19 @@ export class AccountComponent {
                 return body || {};
             })
             .catch(this.handleError);
+    }
+
+    private handleError(error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
     }
 
 }
