7aa60b4a15ec59d04d1c4f954269c934112ac133
Filocha <pafo@gft.com>
2017-06-08 14:47:09 +0200

    Change login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,7 +40,7 @@ import { LogoutService } from './logout/logout.service';
       { path: '**', redirectTo: 'home', pathMatch: 'full' }
     ])
   ],
-  providers: [AuthGuard, LocalStorageService, LoginComponent, LoginService, LogoutService],
+  providers: [AuthGuard, LocalStorageService, LoginComponent, LoginService, LogoutService, ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Headers, RequestOptions } from '@angular/http';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import { FacebookService, InitParams, LoginResponse } from 'ngx-facebook';
-import { LocalStorageService } from 'angular-2-local-storage';
-import { AppComponent } from "../app.component";
+import { LoginService } from '../login/login.service';
+import { AppComponent } from '../app.component';
+
 
 @Component({
     templateUrl: './login.component.html',
@@ -17,64 +10,13 @@ import { AppComponent } from "../app.component";
 
 @Injectable()
 export class LoginComponent implements OnInit {
-    private returnUrl: string;
 
-    constructor(private fb: FacebookService, private http: Http, private router: Router,
-        private route: ActivatedRoute, private localStorageService: LocalStorageService, private app: AppComponent) {
-        this.app.isLogged.next(<boolean>this.localStorageService.get('isLogged'));
-        fb.init({
-            appId: '1722165054742491',
-            xfbml: true,
-            version: 'v2.8'
-        });
+    constructor(private loginService: LoginService, private app: AppComponent) {
     }
 
     ngOnInit() {
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-
-        if (this.localStorageService.get('isLogged') && this.returnUrl === '/') {
-            this.router.navigate(['/home']);
-        }
+        this.app.isLogged.next(true);
+        this.loginService.login();
     }
 
-    login() {
-        this.fb.login()
-            .then((res: LoginResponse) => {
-                console.log(res.authResponse.accessToken);
-                this.sendAccessToken(res.authResponse.accessToken).subscribe(res => {
-                    this.localStorageService.set('isLogged', true);
-                    this.app.isLogged.next(<boolean>this.localStorageService.get('isLogged'));
-                    this.router.navigateByUrl(this.returnUrl);
-                }
-                );
-            })
-            .catch(this.handleError);
-    }
-
-    sendAccessToken(accessToken): Observable<String> {
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers, withCredentials: true });
-
-        return this.http.post('http://localhost:8080/authenticate', { accessToken }, options)
-            .map(this.extractData)
-            .catch(this.handleError);
-    }
-
-    private extractData(res: Response) {
-        let body = res.json();
-        return body.response || {};
-    }
-
-    private handleError(error: Response | any) {
-        let errMsg: string;
-        if (error instanceof Response) {
-            const body = error.json() || '';
-            const err = body.error || JSON.stringify(body);
-            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-        } else {
-            errMsg = error.message ? error.message : error.toString();
-        }
-        console.error(errMsg);
-        return Observable.throw(errMsg);
-    }
 }
