96b6c25db3874852b9f119da33a28f857478c4e1
Filocha <pafo@gft.com>
2017-06-08 14:31:39 +0200

    Refactor

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,23 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import { AuthGuard } from '../guard/authGuard.service';
-import { LocalStorageService } from 'angular-2-local-storage';
-import { AppComponent } from "../app.component";
 
 @Component({
     templateUrl: './account.component.html'
 })
 
 @Injectable()
-export class AccountComponent implements OnInit {
+export class AccountComponent {
     private accountData: String;
     private subscriptions: any = [];
 
-    constructor(private http: Http, private router: Router, private localStorageService: LocalStorageService, private app: AppComponent) { }
+    constructor(private http: Http) { }
 
     ngOnInit() {
         this.getUserSubscriptions().subscribe(res => {
@@ -31,27 +28,6 @@ export class AccountComponent implements OnInit {
         let options = new RequestOptions({ headers: headers, withCredentials: true });
 
         return this.http.get('http://localhost:8080/rest/subscriptions', options)
-            .map(res => {
-                let body = res.json();
-                return body || {};
-            })
-            .catch(this.handleError);
-    }
-
-    logout() {
-        this.logoutUser().subscribe(res => {
-            this.localStorageService.set('isLogged', false);
-            this.app.isLogged.next(false);
-            this.router.navigate(['/home']);
-        }
-        );
-    }
-
-    private logoutUser() {
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers, withCredentials: true });
-
-        return this.http.get('http://localhost:8080/rest/logout', options)
             .map(res => {
                 let body = res.json();
                 return body || {};
